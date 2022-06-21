0a3cb50d6fe0dcbd7fac4008a9a712f084ad53c1
jin-co <lilijk@hanmail.net>
2022-06-21 17:56:26 +0900

    Update

--- index.js
+++ index.js
@@ -31,30 +31,68 @@ function shirinkPanels() {
   })
 }
 
-const info4 = document.querySelector('.img-4-guide-btn')
-const info4Text = document.querySelector('.img-4-guide-text')
-info4.addEventListener('mouseover', () => {
-  console.log('hhh')
-  info4Text.classList.add('show-info')
-})
-
-info4.addEventListener('mouseleave', () => {
-  console.log('hhh')
-  info4Text.classList.remove('show-info')
-})
-
 /** section 3 games (c# an monogame) selection */
 const btnUp = document.querySelector('.btn-panel-up')
 const btnDown = document.querySelector('.btn-panel-down')
+const gameContents = [
+  {
+    bgColor: '#2a86ba',
+    fontColor: '#fff',
+    title: 'mono shooting',
+    description:
+      `<h4>
+        &nbsp;&nbsp;&nbsp;&nbsp;The object of this game is to get to the point without getting hit by enemies
+       </h4>
+       <ol>
+        <p>
+          To run:
+        </p>
+        <li>
+         Download the installer by clicking the game image box 
+        </li>
+        <li>
+         Unzip the file 
+        </li>
+        <li>
+         Find the 'MonoShooting.exe' and run it (double click or right click and click 'open')
+        </li>
+        <li>
+         Enjoy the game
+        </li>
+       </ol>
+      `,
+  },
+  {
+    bgColor: '#fd3555',
+    fontColor: '#fff',
+    title: '',
+    description: '',
+  },
+  {
+    bgColor: '#252e33',
+    fontColor: '#fff',
+    title: '',
+    description: '',
+  },
+  {
+    bgColor: '#ffb866',
+    fontColor: '#fff',
+    title: '',
+    description: '',
+  },
+]
 
 const leftFrames = document.querySelectorAll('.panel-left div')
 const rightFrames = document.querySelectorAll('.panel-right div')
 let frameIndex = 100
+
+// shuffles the slide upward
 btnUp.addEventListener('click', () => {
   frameIndex += 100
   if (frameIndex > 300) {
-    frameIndex = 0    
-  } 
+    frameIndex = 0
+  }
+  addContents(frameIndex)
 
   leftFrames.forEach((frame, idx) => {
     console.log(frame)
@@ -63,14 +101,16 @@ btnUp.addEventListener('click', () => {
 
   rightFrames.forEach((frame) => {
     frame.style.transform = `translateY(-${300 - frameIndex}%)`
-  })  
+  })
 })
 
+// shuffles the slide downward
 btnDown.addEventListener('click', () => {
   frameIndex -= 100
   if (frameIndex < 0) {
-    frameIndex = 300    
-  } 
+    frameIndex = 300
+  }
+  addContents(frameIndex)
 
   leftFrames.forEach((frame, idx) => {
     console.log(frame)
@@ -79,8 +119,31 @@ btnDown.addEventListener('click', () => {
 
   rightFrames.forEach((frame) => {
     frame.style.transform = `translateY(-${300 - frameIndex}%)`
-  })  
+  })
 })
+
+const info = document.querySelector('.img-guide-btn')
+const infoText = document.querySelector('.img-guide-text')
+const infoBtn = document.querySelector('.img-guide-btn')
+info.addEventListener('mouseover', () => {
+  infoText.classList.add('show-info')  
+})
+
+infoText.addEventListener('mouseleave', () => {
+  infoText.classList.remove('show-info')
+})
+
+addContents()
+
+// adds game content to guide box to show related content description
+function addContents(frameIdx = 0) {
+  console.log('fram index: ', frameIdx)
+  const index = frameIdx / 100
+  infoText.innerHTML = gameContents[`${index}`].description
+  infoText.style.backgroundColor = `${gameContents[index].bgColor}`
+  infoText.style.color = `${gameContents[index].fontColor}` 
+  infoBtn.style.backgroundColor = `${gameContents[index].bgColor}`
+}
 
 /** common */
 const arrowBoxes = document.querySelectorAll('.arrow-box')
